07c5341c67448b6b0f05e3b03f49392dde0bcafc
Pavel-Kolenda <progap0002@gmail.com>
2024-05-25 19:46:50 +0300

    Remove boilerplate code in app.component

--- DentalClinic.UI/src/app/app.component.ts
+++ DentalClinic.UI/src/app/app.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
+import {HttpClientModule} from "@angular/common/http";
+import {BrowserModule} from "@angular/platform-browser";
+import {HeaderComponent} from "./core/layout/header/header.component";
+import {FooterComponent} from "./core/layout/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    RouterLink,
+    RouterLinkActive,
+    HttpClientModule,
+    HeaderComponent,
+    FooterComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'DentalClinic.UI';
 }
