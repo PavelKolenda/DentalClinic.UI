2b4a0c8c2d0bb293cc100924ce11a8966d9b74dc
Pavel-Kolenda <progap0002@gmail.com>
2024-05-28 14:37:00 +0300

    Add routes

--- DentalClinic.UI/src/app/app.component.ts
+++ DentalClinic.UI/src/app/app.component.ts
@@ -1,23 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
-import {HttpClientModule} from "@angular/common/http";
-import {BrowserModule} from "@angular/platform-browser";
-import {HeaderComponent} from "./core/layout/header/header.component";
+import {HttpClient} from "@angular/common/http";
 import {FooterComponent} from "./core/layout/footer/footer.component";
+import {AuthService} from "./core/auth/services/auth.service";
+import {JwtService} from "./core/auth/services/jwt.service";
+import {AuthResponse} from "./core/auth/models/AuthResponse";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    RouterOutlet,
-    RouterLink,
-    RouterLinkActive,
-    HttpClientModule,
-    HeaderComponent,
-    FooterComponent,
-  ],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, FooterComponent,],
+  providers: [AuthService, JwtService,],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  constructor(private authService: AuthService, private httpClient: HttpClient) {
+
+  }
+
+  ngOnInit(): void {
+    this.httpClient.get<AuthResponse>('https://localhost:7098/api/auth')
+      .subscribe((response) => {
+        this.authService.currentUserSignal.set(response);
+      });
+  }
 }
