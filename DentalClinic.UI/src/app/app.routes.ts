38fa9b92eb1fe45bac84f0cb917537b29f2fa9ba
Pavel-Kolenda <progap0002@gmail.com>
2024-06-03 16:56:34 +0300

    Add route to AppointmentDetails

--- DentalClinic.UI/src/app/app.routes.ts
+++ DentalClinic.UI/src/app/app.routes.ts
@@ -1,8 +1,27 @@
 import { Routes } from '@angular/router';
 import {RegistrationComponent} from "./core/auth/registration/registration.component";
 import {LoginComponent} from "./core/auth/login/login.component";
+import {
+  SelectSpecializationComponent
+} from "./features/appointments/pages/select-specialization/select-specialization.component";
+import {SelectDentistComponent} from "./features/appointments/pages/select-dentist/select-dentist.component";
+import {
+  SelectAppointmentComponent
+} from "./features/appointments/pages/select-appointment/select-appointment.component";
+import {
+  AppointmentDetailsComponent
+} from "./features/appointments/pages/appointment-details/appointment-details.component";
+import {isLoggedGuard} from "./is-logged.guard";
 
 export const routes: Routes = [
   { path: 'registration', component: RegistrationComponent },
-  { path: 'login', component: LoginComponent }
+  { path: 'login', component: LoginComponent },
+  { path: 'appointment', component: SelectSpecializationComponent },
+  {
+    path: 'appointment/details/:appointmentId',
+    component: AppointmentDetailsComponent,
+    canActivate: [isLoggedGuard]
+  },
+  { path: 'appointment/:specializationId', component: SelectDentistComponent },
+  { path: 'appointment/:specializationId/:dentistId', component: SelectAppointmentComponent }
 ];
