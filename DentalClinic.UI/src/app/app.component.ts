da31b85b5500887b246819b23a4f887c490519db
Pavel-Kolenda <progap0002@gmail.com>
2024-06-03 16:52:49 +0300

    Update AppComponent

--- DentalClinic.UI/src/app/app.component.ts
+++ DentalClinic.UI/src/app/app.component.ts
@@ -1,31 +1,20 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
-import {HttpClient} from "@angular/common/http";
 import {FooterComponent} from "./core/layout/footer/footer.component";
 import {AuthService} from "./core/auth/services/auth.service";
 import {JwtService} from "./core/auth/services/jwt.service";
-import {AuthResponse} from "./core/auth/models/AuthResponse";
 import {HeaderComponent} from "./core/layout/header/header.component";
 import {AppointmentService} from "./features/appointments/services/appointment.service";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive, FooterComponent, HeaderComponent ],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, FooterComponent, HeaderComponent],
   providers: [AuthService, JwtService, AppointmentService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
 
-  constructor(private authService: AuthService, private httpClient: HttpClient) {
-
-  }
-
-  ngOnInit(): void {
-    this.httpClient.get<AuthResponse>('https://localhost:7098/api/auth')
-      .subscribe((response) => {
-        this.authService.currentUserSignal.set(response);
-      });
-  }
+  constructor(){}
 }
