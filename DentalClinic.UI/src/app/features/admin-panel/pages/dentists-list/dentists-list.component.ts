f2b89e81a79e89dc4cdde1fbbf5b8d5b7656bc0f
Pavel-Kolenda <progap0002@gmail.com>
2024-06-08 11:54:41 +0300

    Add dentist list component

---
+++ DentalClinic.UI/src/app/features/admin-panel/pages/dentists-list/dentists-list.component.ts
@@ -0,0 +1,41 @@
+import {Component, OnInit} from '@angular/core';
+import {AdminService} from "../../services/admin.service";
+import {Router} from "@angular/router";
+import {PagedRequest} from "../../../../shared/models/paged-request";
+import {PagedResponse} from "../../../../shared/models/paged-response";
+import {Dentist} from "../../../appointments/models/dentist";
+import {FormsModule} from "@angular/forms";
+import {SortService} from "../../../../shared/services/sort.service";
+import { ListComponent } from '../../../../shared/components/list.component';
+
+@Component({
+  selector: 'app-dentists-list',
+  standalone: true,
+  imports: [
+    FormsModule
+  ],
+  templateUrl: './dentists-list.component.html',
+  styleUrl: './dentists-list.component.css'
+})
+export class DentistsListComponent extends ListComponent<Dentist>  {
+
+  constructor(private adminService: AdminService, private router: Router, sortService: SortService) {
+    super(sortService);
+  }
+
+  protected getItems(): void {
+    this.adminService.getDentist(this.pagedRequest).subscribe((response) => {
+      this.items = response;
+    });
+  }
+
+  public deleteDentist(id: number){
+    if (window.confirm('Вы уверены что хотите удалить?')) {
+      this.adminService.deleteDentist(id).subscribe((response) => {
+        this.getItems();
+      }, (error) => {
+        console.error('Error deleting dentist:', error);
+      });
+    }
+  }
+}
