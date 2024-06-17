565eee547ce48d4fb1188d34a318008ab314e7f9
Pavel-Kolenda <progap0002@gmail.com>
2024-06-17 21:51:28 +0300

    Add Dentist Update model

---
+++ DentalClinic.UI/src/app/features/admin-panel/models/dentist/dentist-as-user.model.ts
@@ -0,0 +1,11 @@
+export interface DentistAsUserModel{
+  name: string;
+  surname: string;
+  patronymic: string;
+  cabinetNumber: string;
+  specialization: string;
+  email: string;
+  birthDate: string;
+  address: string;
+  phoneNumber: string;
+}
---
+++ DentalClinic.UI/src/app/features/admin-panel/models/dentist/dentist-update-model.ts
@@ -0,0 +1,12 @@
+export interface DentistUpdateModel{
+  name: string;
+  surname: string;
+  patronymic: string;
+  cabinetNumber: string;
+  specialization: string;
+  email: string;
+  password?: string;
+  birthDate: string;
+  address: string;
+  phoneNumber: string;
+}
