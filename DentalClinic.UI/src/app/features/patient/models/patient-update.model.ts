314f94ed48c47ef427284ae6c70fbd6e15f72245
Pavel-Kolenda <progap0002@gmail.com>
2024-06-18 23:21:40 +0300

    Add patient-update.model

---
+++ DentalClinic.UI/src/app/features/patient/models/patient-update.model.ts
@@ -0,0 +1,10 @@
+export interface PatientUpdateModel{
+  name: string;
+  surname: string;
+  patronymic: string;
+  email: string;
+  password: string;
+  birthDate: string;
+  address: string;
+  phoneNumber: string;
+}
